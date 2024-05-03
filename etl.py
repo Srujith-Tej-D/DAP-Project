678c8e0c89951a1e7c8c43e4e745d039bcd97ff8
Srujith-Tej-D <157812907+Srujith-Tej-D@users.noreply.github.com>
2024-05-03 11:49:51 +0100

    ETL pipeline final code

--- etl.py
+++ etl.py
@@ -4,26 +4,9 @@ from datasets import *
 from transform_n_load import *
 
 
-<<<<<<< Updated upstream
-
-@graph
-def etl_pipeline():
-    downloaded_data = saving_datasets()
-    ds1, ds2 = ingesting_violations_json_to_mongo(downloaded_data), ingesting_crashes_csv_to_mongo(downloaded_data)
-
-etl_job = etl_pipeline.to_job()
-
-@repository
-def my_etl_repository():
-    """
-    Repository to hold ETL jobs for managing data extraction and ingestion.
-    """
-    return [etl_job]
-=======
 @job
 def my_data_pipeline():
     downloaded_data = saving_datasets()
     ds1, ds2 = ingesting_violations_json_to_mongo(downloaded_data), ingesting_crashes_csv_to_mongo(downloaded_data)
     violations_df, traffic_df = mongo_extraction(ds1, ds2)
     transform_and_load(violations_df, traffic_df)
->>>>>>> Stashed changes
