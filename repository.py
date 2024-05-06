53f0c5927603591b1c4b592f2168ee454551da68
Srujith-Tej-D <157812907+Srujith-Tej-D@users.noreply.github.com>
2024-05-06 13:06:52 +0100

    Delete repository.py

--- repository.py
+++
@@ -1,9 +0,0 @@
-from dagster import repository
-from etl import *
-
-@repository
-def my_etl_repository():
-    """
-    Repository to hold ETL jobs for managing data extraction and ingestion.
-    """
-    return [downloading_datasets ,loading_dataset_1, loading_dataset_2]
